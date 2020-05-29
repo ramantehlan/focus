cdf21495ff0609f922e3b908dc350ea30ce6247f
Raman Tehlan <ramantehlan@gmail.com>
2020-05-30 05:12:48 +0530

    First beta

---
+++ README.md
@@ -0,0 +1,4 @@
+# Focus
+
+A simple CLI tool to set task you want to focus on. I wrote this small program to work with pomodoro and set the focus for the given time. I use it in the i3status line. 
+
--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,8 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "focus",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Set the focus for pomodoro",
+	Long:  `This is a very simple application to set the focus for your pomodoro time.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
